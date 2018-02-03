027f2b29d730179ee89c7bef0d709426a14dafaa
Donald Gregorich <dgregorich@gmail.com>
2018-02-03 16:45:59 -0500

    Spice api test, added requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from app import app
+import spice_api
+import random
 
 @app.route('/')
 @app.route('/index')
 def index():
+	'''
 	anime1 = {
 		'title': 'Cowboy Bebop',
 		'image': 'https://myanimelist.cdn-dena.com/images/anime/4/19644.jpg'
@@ -12,4 +15,16 @@ def index():
 		'title': 'Toradora!',
 		'image': 'https://myanimelist.cdn-dena.com/images/anime/13/22128.jpg'
 	}
-	return render_template('index.html', title='Home', anime=anime1)
+	'''
+	creds = spice_api.load_auth_from_file('C:\\Users\\dgreg\\Documents\\Programming\\anirank\\app\\auth.txt')
+	anime_ids = [32828, 22147, 6547, 9062]
+	anime1 = spice_api.search_id(random.choice(anime_ids),spice_api.get_medium("anime"),creds)
+	anime2 = spice_api.search_id(random.choice(anime_ids),spice_api.get_medium("anime"),creds)
+	return render_template('index.html', title='Home', anime1=anime1, anime2=anime2)
+	
+'''
+>>> import datetime
+>>> dt=time.strptime('2016-07-05', '%Y-%m-%d')
+>>> f'{dt:%A} {dt:%B} {dt.day}, {dt.year}'
+'Monday August 29, 2016'
+'''
