e56aa97f53d77c552bc805f45ccee1fdafabb583
Donald Gregorich <dgregorich@gmail.com>
2018-03-03 13:18:35 -0500

    Graphical updates to comparison screen

--- app/routes.py
+++ app/routes.py
@@ -3,23 +3,29 @@ from app import app
 from app.forms import LoginForm, OptionsForm
 import spice_api
 import random
+import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 
 	anime1 = {
-		'title': 'Cowboy Bebop',
-		'english': 'Cowboy Bebop',
+		'id': 34798,
+		'title': 'Yuru Camp△',
+		'english': 'Laid-Back Camp',
 		'dates': ('1854-01-01', '1925-01-01'),
-		'image_url': 'https://myanimelist.cdn-dena.com/images/anime/4/19644.jpg'
+		'image_url': 'https://myanimelist.cdn-dena.com/images/anime/4/89877.jpg'
 	}
 	anime2 = {
+		'id': 4224,
 		'title': 'Toradora!',
 		'english': 'Toradora!',
 		'dates': ('1574-01-01', '1831-01-01'),
 		'image_url': 'https://myanimelist.cdn-dena.com/images/anime/13/22128.jpg'
 	}
+	for anime in [anime1,anime2]:
+		dt = datetime.datetime.strptime(anime['dates'][0],'%Y-%m-%d')
+		anime['dateFormat'] = f'{dt:%B} {dt.day}, {dt.year}'
 	'''
 	creds = spice_api.load_auth_from_file('C:\\Users\\dgreg\\Documents\\Programming\\anirank\\app\\auth.txt')
 	anime_ids = [32828, 22147, 6547, 9062]
@@ -31,12 +37,8 @@ def index():
 		flash('Options selected!')
 		return redirect(url_for('index'))
 	return render_template('index.html', title='Home', anime1=anime1, anime2=anime2, form=form)
-	'''
-	>>> import datetime
-	>>> dt=time.strptime('2016-07-05', '%Y-%m-%d')
-	>>> f'{dt:%A} {dt:%B} {dt.day}, {dt.year}'
-	'Monday August 29, 2016'
-	'''
+	
+	
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
